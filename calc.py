4e8af948e9b1f97d5f2c3756d111a47547dfa640
sanuwanihewa <sanuwani.hewamunasinghe@monash.edu>
2023-03-21 14:34:56 +1100

    added divison

--- calc.py
+++ calc.py
@@ -3,8 +3,6 @@ def compute(expression):
     num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return int(num0) + int(num1)
-    elif operator == '-':
-        return int(num0) - int(num1)
     elif operator == '/':
         return int(num0) / int(num1)
     else:
