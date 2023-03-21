bfcc5fd9caff47e0ed3ac58fd0a5d5402bc190da
sanuwanihewa <sanuwani.hewamunasinghe@monash.edu>
2023-03-21 14:18:22 +1100

    updated calc

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     if operator == '+':
-        return num0 + num1
+        return int(num0) + int(num1)
     elif operator == '-':
-        return num0 - num1
+        return int(num0) - int(num1)
     else:
         print('unknown operator!')
         return None
